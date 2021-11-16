9156fa89adf76b0f73f89b2291d89b5bde05461f
CaesarHp <hp88502225@gmail.com>
2021-11-15 20:20:27 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./ui/Header";
+
+const DOMMY_DATA = {
+  id: 7834,
+  created_at: "2018-04-19T09:38:41.000Z",
+  direction: "outbound",
+  from: "Pierre-Baptiste Béchu",
+  to: "06 46 62 12 33",
+  via: "NYC Office",
+  duration: "120",
+  is_archived: false,
+  call_type: "missed",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Header className="container" />
+        <div className="container-view">Hello</div>
+      </div>
+    </>
   );
 }
 
